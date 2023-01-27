3b0485cd33920a02b06f51600382ec08c1e03ccc
Kirill S <ksemasko@tktk.ee>
2023-01-27 14:31:03 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿//This is a comment to my code
+//rakendus küsib kasutajal sisestada tema nime 
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");//Output
+
+// stirng - sõnad (ainult sõnad) 
+
+string userName = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Hello" + ", " + userName + "!");// Output
+// string interpolation
+Console.WriteLine($"Hello, {userName}!");//Output
+
+
+
+
